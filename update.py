e95f7648a505d01c326af7dcf8ca24de19493f36
Braden Hoagland <bhoggland@gmail.com>
2020-04-02 16:30:11 -0400

    HJB and progress on WGF

--- update.py
+++ update.py
@@ -10,19 +10,20 @@ end -= end - start - 1
 
 # loop through files to link to
 for filename in os.listdir('notes'):
-    name = filename.replace('.html', '')
+    if '.html' in filename:
+        name = filename.replace('.html', '')
 
-    # find file title
-    with open(f'notes/{filename}', 'r') as f:
-        text = f.read()
-        i = text.index('<title>') + 7
-        j = text.index('</title>')
-        title = text[i : j]
+        # find file title
+        with open(f'notes/{filename}', 'rb') as f:
+            text = str(f.read())
+            i = text.index('<title>') + 7
+            j = text.index('</title>')
+            title = text[i : j]
 
-    # insert new link
-    lines.insert(end, '    </div>\n')
-    lines.insert(end, f'        <a href="notes/{name}.html">{title}</a>\n')
-    lines.insert(end, '    <div class="item">\n')
+        # insert new link
+        lines.insert(end, '    </div>\n')
+        lines.insert(end, f'        <a href="notes/{name}.html">{title}</a>\n')
+        lines.insert(end, '    <div class="item">\n')
 
 # update index.html file
 text = ''.join(lines)
